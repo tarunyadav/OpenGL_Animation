8c6dc2da6153d8a411ebd84239fc55013ca22f5c
Tarun Yadav <tarunyadav.iitp@gmail.com>
2011-11-20 23:28:26 -0800

    Fountain added

---
+++ Fountain/textures.cpp
@@ -0,0 +1,119 @@
+#include "textures.h"
+
+
+int texture[1];
+int COGLTexture::ImageLoad(char *filename, Image *image)
+{
+    FILE *file;
+    unsigned long size;                 // size of the image in bytes.
+    unsigned long i;                    // standard counter.
+    unsigned short int planes;          // number of planes in image (must be 1)
+    unsigned short int bpp;             // number of bits per pixel (must be 24)
+    char temp;                          // temporary color storage for bgr-rgb conversion.
+
+    // make sure the file is there.
+    if ((file = fopen(filename, "rb"))==NULL)
+    {
+	printf("File Not Found : %s\n",filename);
+	return 0;
+    }
+
+    // seek through the bmp header, up to the width/height:
+    fseek(file, 18, SEEK_CUR);
+
+    // read the width
+    if ((i = fread(&image->sizeX, 4, 1, file)) != 1) {
+	printf("Error reading width from %s.\n", filename);
+	return 0;
+    }
+    printf("Width of %s: %lu\n", filename, image->sizeX);
+
+    // read the height
+    if ((i = fread(&image->sizeY, 4, 1, file)) != 1) {
+	printf("Error reading height from %s.\n", filename);
+	return 0;
+    }
+    printf("Height of %s: %lu\n", filename, image->sizeY);
+
+    // calculate the size (assuming 24 bits or 3 bytes per pixel).
+    size = image->sizeX * image->sizeY * 3;
+
+    // read the planes
+    if ((fread(&planes, 2, 1, file)) != 1) {
+	printf("Error reading planes from %s.\n", filename);
+	return 0;
+    }
+    if (planes != 1) {
+	printf("Planes from %s is not 1: %u\n", filename, planes);
+	return 0;
+    }
+
+    // read the bpp
+    if ((i = fread(&bpp, 2, 1, file)) != 1) {
+	printf("Error reading bpp from %s.\n", filename);
+	return 0;
+    }
+    if (bpp != 24) {
+	printf("Bpp from %s is not 24: %u\n", filename, bpp);
+	return 0;
+    }
+
+    // seek past the rest of the bitmap header.
+    fseek(file, 24, SEEK_CUR);
+
+    // read the data.
+    image->data = (char *) malloc(size);
+    if (image->data == NULL) {
+	printf("Error allocating memory for color-corrected image data");
+	return 0;
+    }
+
+    if ((i = fread(image->data, size, 1, file)) != 1) {
+	printf("Error reading image data from %s.\n", filename);
+	return 0;
+    }
+
+    for (i=0;i<size;i+=3) { // reverse all of the colors. (bgr -> rgb)
+	temp = image->data[i];
+	image->data[i] = image->data[i+2];
+	image->data[i+2] = temp;
+    }
+
+    // we're done.
+    return 1;
+
+}
+void COGLTexture::LoadFromFile(char *filename) {
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1,&ID);
+	glBindTexture( GL_TEXTURE_2D, ID);
+
+	Image *Image1;
+	Image1 = (Image *) malloc(sizeof(Image));
+	ImageLoad(filename,Image1);
+	//Image1 = auxDIBImageLoadA( (const char*) filename );
+	Width = Image1->sizeX;
+	Height = Image1->sizeY;
+	//printf("here %s",Image1->data);
+	gluBuild2DMipmaps(	GL_TEXTURE_2D,3,Image1->sizeX,Image1->sizeY,GL_RGB,GL_UNSIGNED_BYTE,Image1->data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	delete Image1;
+};
+
+void COGLTexture::SetActive()
+{
+	glBindTexture( GL_TEXTURE_2D, ID);
+}
+
+unsigned int COGLTexture::GetID()
+{
+	return ID;
+}
+
---
+++ Fountain/textures.h
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+#include <stdio.h>      // Header file for standard file i/o.
+#include <stdlib.h>     // Header file for malloc/free.
+#include <unistd.h>     // needed to sleep.
+
+
+
+/* Image type - contains height, width, and data */
+struct Image {
+    unsigned long sizeX;
+    unsigned long sizeY;
+    char *data;
+
+};
+typedef struct Image Image;
+class COGLTexture
+{
+public:
+/*	_AUX_RGBImageRec *Image;*/
+	unsigned int GetID();
+	void LoadFromFile(char *filename);
+	int ImageLoad (char *filename,Image *Image);
+
+	void SetActive();
+	int GetWidth();
+	int GetHeight();
+private:
+	int Width, Height;
+	unsigned int ID;
+};
+
